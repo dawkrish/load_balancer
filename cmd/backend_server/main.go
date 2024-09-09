90d57f15390f07e4d704b88428fa274944db5372
dawkrish <krishnansh710@gmail.com>
2024-09-09 13:52:13 +0530

    our go server

--- cmd/backend_server/main.go
+++ cmd/backend_server/main.go
@@ -9,8 +9,15 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		metadata := fmt.Sprintf("Received request from %v\n", r.Host) +
+			fmt.Sprintf("%v %v %v\n", r.Method, r.URL, r.Proto) +
+			fmt.Sprintf("Host: %v\n", r.Host) +
+			fmt.Sprintf("User-Agent: %v\n", r.Header.Get("User-Agent")) +
+			fmt.Sprintf("Accept: %v\n", r.Header.Get("Accept"))
+		fmt.Println(metadata)
+
 		fmt.Fprintf(w, "Hello From Backend Server")
 	})
 
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	log.Fatal(http.ListenAndServe(":7842", nil))
 }
